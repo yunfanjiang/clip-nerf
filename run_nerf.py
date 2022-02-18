f43b6945a7006e73da5166e71c0cd99cfcc21d9e
Yunfan Jiang <yunfanjyf@gmail.com>
2022-02-17 21:43:37 -0800

    auto device

--- run_nerf.py
+++ run_nerf.py
@@ -873,6 +873,6 @@ def train():
 
 
 if __name__=='__main__':
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
-
+    default_tensor_type = 'torch.cuda.FloatTensor' if torch.cuda.is_available() else 'torch.FloatTensor'
+    torch.set_default_tensor_type(default_tensor_type)
     train()
